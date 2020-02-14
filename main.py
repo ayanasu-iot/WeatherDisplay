1074240d47ea64e3a822f46231cb536e9170420a
Kanta Demizu <demizu.1203@gmail.com>
2020-02-14 14:28:16 +0900

    :sparkles: Added Image files

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import requests
 import subprocess
 
 
-def download_img(url, file_name):
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        with open(file_name, 'wb') as f:
-            f.write(r.content)
-
-
 def main():
     url = API_BASEURL + API_KEY
     response = requests.get(url)
@@ -18,11 +11,9 @@ def main():
     res = response.json()
     json_data = json.dumps(res, indent=4)
     print(json_data)
-    icon_url = ICON_BASEURL + res['weather'][0]['icon'] + "@2x.png"
+    icon_uri = './images/' + res['weather'][0]['main'] + ".png"
 
-    download_img(icon_url, 'icon.png')
-
-    subprocess.run(['/usr/local/bin/led-image-viewer', 'icon.png', '--led-slowdown-gpio=2'])
+    subprocess.run(['/usr/local/bin/led-image-viewer', icon_uri, '--led-slowdown-gpio=2'])
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -9,4 +9,3 @@ load_dotenv(dotenv_path)
 
 API_KEY = os.environ.get("API_KEY")
 API_BASEURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&id=1857766&appid="
-ICON_BASEURL = "http://openweathermap.org/img/wn/"
